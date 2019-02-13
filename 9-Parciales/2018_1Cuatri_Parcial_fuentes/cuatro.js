7058e98bb9f0ceacf6faa1f7411582714f0048b3
alumno <alumno>
2019-02-13 11:33:30 -0300

    cuatro.js parcial

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
@@ -1,4 +1,36 @@
 function mostrar()
 {
+var productos;
+var precio;
+var precio2;
+var formadepago;
+tarjeta = parseInt(tarjeta);
+precio = parseInt(precio);
+var tarjeta = (10*precio)/100;
+
+
+
+
+
+productos= prompt("¿Cuantos productos compro?");
+precio= prompt("ingrese el valor del producto");
+formadepago= prompt("¿efectivo o tarjeta?");
+
+
+if((productos = 1) && (formadepago == "tarjeta"))
+	{
+		alert("debe pagar con recargo " + tarjeta);
+	}
+if((productos = 1) && (formadepago == "efectivo"))
+
+{
+
+alert("debe pagar "+ precio);
+	
+}
+
+
+
+
 
 }
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
@@ -1,5 +1,23 @@
 
 function mostrar()
 {
+var persona1;
+var persona2;
+var edad1;
+var edad2;
+var sumaedad;
+
+persona1 = prompt("ingrese su nombre");
+edad1 = prompt("ingrese su edad");
+persona2 = prompt("ingrese su nombre");
+edad2 = prompt("ingrese su edad");
+
+edad1 = parseInt(edad1);
+edad2 = parseInt(edad2);
+
+
+sumaedad = (edad1 + edad2);
+
+alert("ustedes son "+ persona1 + " y "+ persona2 + " y sus edades son " + edad1 + " y " +edad2+ ", y la suma de sus edades es " + sumaedad);
 
 }
